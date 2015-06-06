64d4faa304d987edfc51e5e6ea1f6382a40244ac
pigsdofly <pigsdofly6@gmail.com>
2015-06-06 01:12:55 +0100

    ANIMATIONS

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ window.onresize = function(event) {
 	resize();
 }
 
+window.onscroll = function(event) {
+    $('#content').css({'transform':'translateY(-100%)','transition':'transform 200ms ease-in'});
+}
+
 function resize() {
 	$('#content').css({'top':$(window).height()});
 	console.log("css changed");
